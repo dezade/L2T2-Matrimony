265e0644c131b72e3e26c0565dffe9376ff72277
TowfiqueMahmud <towfiquemahmud6@gmail.com>
2023-09-12 00:51:09 +0600

    Update.js done, onclick of company location commented out

--- frontend/src/components/Update.js
+++ frontend/src/components/Update.js
@@ -20,6 +20,8 @@ function Update() {
   const [thana, setThana] = useState("");
   const [profession, setProfession] = useState("");
   const [companyLocation, setCompanyLocation] = useState("");
+  const [companyDistrict, setCompanyDistrict] = useState("");
+  const [companyThana, setCompanyThana] = useState("");
   const [educationLevel, setEducationLevel] = useState("");
   const [subject, setSubject] = useState("");
   const [institution, setInstitution] = useState("");
@@ -31,6 +33,87 @@ function Update() {
   const [isSchoolDisabled, setIsSchoolDisabled] = useState(true);
   const [isSubjectDisabled, setIsSubjectDisabled] = useState(true);
   const [availableSchools, setAvailableSchools] = useState([]);
+  const [isThanaDisabled, setIsThanaDisabled] = useState(true);
+  const [isCompanyThanaDisabled, setIsCompanyThanaDisabled] = useState(true);
+  const [availableThana, setAvailableThana] = useState([]);
+  const [availableCompanyThana, setAvailableCompanyThana] = useState([]);
+
+  const availableDistrict = ['Cumilla', 'Feni', 'Brahmanbaria', 'Rangamati', 'Noakhali', 'Chandpur', 'Lakshmipur',
+    'Chattogram', 'Coxsbazar', 'Khagrachhari', 'Bandarban', 'Sirajganj', 'Pabna', 'Bogura', 'Rajshahi',
+    'Natore', 'Joypurhat', 'Chapainawabganj', 'Naogaon', 'Jashore', 'Satkhira', 'Meherpur', 'Narail',
+    'Chuadanga', 'Kushtia', 'Magura', 'Khulna', 'Bagerhat', 'Jhenaidah', 'Jhalakathi', 'Patuakhali',
+    'Pirojpur', 'Barishal', 'Bhola', 'Barguna', 'Sylhet', 'Moulvibazar', 'Habiganj', 'Sunamganj',
+    'Narsingdi', 'Gazipur', 'Shariatpur', 'Narayanganj', 'Tangail', 'Kishoreganj', 'Manikganj', 'Dhaka',
+    'Munshiganj', 'Rajbari', 'Madaripur', 'Gopalganj', 'Faridpur', 'Panchagarh', 'Dinajpur', 'Lalmonirhat',
+    'Nilphamari', 'Gaibandha', 'Thakurgaon', 'Rangpur', 'Kurigram', 'Sherpur', 'Mymensingh', 'Jamalpur',
+    'Netrokona'];
+  const allowedThanaByDistrict = {
+    Cumilla: ['Debidwar', 'Barura', 'Brahmanpara', 'Chandina', 'Chauddagram', 'Daudkandi', 'Homna', 'Laksam', 'Muradnagar', 'Nangalkot', 'Cumillasadar', 'Meghna', 'Monohargonj', 'Sadarsouth', 'Titas', 'Burichang', 'Lalmai'],
+    Feni: ['Chhagalnaiya', 'Sadar', 'Sonagazi', 'Fulgazi', 'Parshuram', 'Daganbhuiyan'],
+    Brahmanbaria: ['Sadar', 'Kasba', 'Nasirnagar', 'Sarail', 'Ashuganj', 'Akhaura', 'Nabinagar', 'Bancharampur', 'Bijoynagar'],
+    Rangamati: ['Sadar', 'Kaptai', 'Kawkhali', 'Baghaichari', 'Barkal', 'Langadu', 'Rajasthali', 'Belaichari', 'Juraichari', 'Naniarchar'],
+    Noakhali: ['Sadar', 'Companiganj', 'Begumganj', 'Hatia', 'Subarnachar', 'Kabirhat', 'Senbug', 'Chatkhil', 'Sonaimuri'],
+    Chandpur: ['Haimchar', 'Kachua', 'Shahrasti', 'Sadar', 'Matlabsouth', 'Hajiganj', 'Matlabnorth', 'Faridgonj'],
+    Lakshmipur: ['Sadar', 'Kamalnagar', 'Raipur', 'Ramgati', 'Ramganj'],
+    Chattogram: ['Rangunia', 'Sitakunda', 'Mirsharai', 'Patiya', 'Sandwip', 'Banshkhali', 'Boalkhali', 'Anwara', 'Chandanaish', 'Satkania', 'Lohagara', 'Hathazari', 'Fatikchhari', 'Raozan', 'Karnafuli'],
+    Coxsbazar: ['Sadar', 'Chakaria', 'Kutubdia', 'Ukhiya', 'Moheshkhali', 'Pekua', 'Ramu', 'Teknaf'],
+    Khagrachhari: ['Sadar', 'Dighinala', 'Panchari', 'Laxmichhari', 'Mohalchari', 'Manikchari', 'Ramgarh', 'Matiranga', 'Guimara'],
+    Bandarban: ['Sadar', 'Alikadam', 'Naikhongchhari', 'Rowangchhari', 'Lama', 'Ruma', 'Thanchi'],
+    Sirajganj: ['Belkuchi', 'Chauhali', 'Kamarkhand', 'Kazipur', 'Raigonj', 'Shahjadpur', 'Sirajganjsadar', 'Tarash', 'Ullapara'],
+    Pabna: ['Sujanagar', 'Ishurdi', 'Bhangura', 'Pabnasadar', 'Bera', 'Atghoria', 'Chatmohar', 'Santhia', 'Faridpur'],
+    Bogura: ['Kahaloo', 'Sadar', 'Shariakandi', 'Shajahanpur', 'Dupchanchia', 'Adamdighi', 'Nondigram', 'Sonatala', 'Dhunot', 'Gabtali', 'Sherpur', 'Shibganj'],
+    Rajshahi: ['Paba', 'Durgapur', 'Mohonpur', 'Charghat', 'Puthia', 'Bagha', 'Godagari', 'Tanore', 'Bagmara'],
+    Natore: ['Natoresadar', 'Singra', 'Baraigram', 'Bagatipara', 'Lalpur', 'Gurudaspur', 'Naldanga'],
+    Joypurhat: ['Akkelpur', 'Kalai', 'Khetlal', 'Panchbibi', 'Joypurhatsadar'],
+    Chapainawabganj: ['Chapainawabganjsadar', 'Gomostapur', 'Nachol', 'Bholahat', 'Shibganj'],
+    Naogaon: ['Mohadevpur', 'Badalgachi', 'Patnitala', 'Dhamoirhat', 'Niamatpur', 'Manda', 'Atrai', 'Raninagar', 'Naogaonsadar', 'Porsha', 'Sapahar'],
+    Jashore: ['Manirampur', 'Abhaynagar', 'Bagherpara', 'Chougachha', 'Jhikargacha', 'Keshabpur', 'Sadar', 'Sharsha'],
+    Satkhira: ['Assasuni', 'Debhata', 'Kalaroa', 'Satkhirasadar', 'Shyamnagar', 'Tala', 'Kaliganj'],
+    Meherpur: ['Mujibnagar', 'Meherpursadar', 'Gangni'],
+    Narail: ['Narailsadar', 'Lohagara', 'Kalia'],
+    Chuadanga: ['Chuadangasadar', 'Alamdanga', 'Damurhuda', 'Jibannagar'],
+    Kushtia: ['Kushtiasadar', 'Kumarkhali', 'Khoksa', 'Mirpurkushtia', 'Daulatpur', 'Bheramara'],
+    Magura: ['Shalikha', 'Sreepur', 'Magurasadar', 'Mohammadpur'],
+    Khulna: ['Paikgasa', 'Fultola', 'Digholia', 'Rupsha', 'Terokhada', 'Dumuria', 'Botiaghata', 'Dakop', 'Koyra'],
+    Bagerhat: ['Fakirhat', 'Sadar', 'Mollahat', 'Sarankhola', 'Rampal', 'Morrelganj', 'Kachua', 'Mongla', 'Chitalmari'],
+    Jhenaidah: ['Sadar', 'Shailkupa', 'Harinakundu', 'Kaliganj', 'Kotchandpur', 'Moheshpur'],
+    Jhalakathi: ['Sadar', 'Kathalia', 'Nalchity', 'Rajapur'],
+    Patuakhali: ['Bauphal', 'Sadar', 'Dumki', 'Dashmina', 'Kalapara', 'Mirzaganj', 'Galachipa', 'Rangabali'],
+    Pirojpur: ['Sadar', 'Nazirpur', 'Kawkhali', 'Bhandaria', 'Mathbaria', 'Nesarabad', 'Indurkani'],
+    Barishal: ['Barishalsadar', 'Bakerganj', 'Babuganj', 'Wazirpur', 'Banaripara', 'Gournadi', 'Agailjhara', 'Mehendiganj', 'Muladi', 'Hizla'],
+    Bhola: ['Sadar', 'Borhanuddin', 'Charfesson', 'Doulatkhan', 'Monpura', 'Tazumuddin', 'Lalmohan'],
+    Barguna: ['Amtali', 'Sadar', 'Betagi', 'Bamna', 'Pathorghata', 'Taltali'],
+    Sylhet: ['Balaganj', 'Beanibazar', 'Bishwanath', 'Companiganj', 'Fenchuganj', 'Golapganj', 'Gowainghat', 'Jaintiapur', 'Kanaighat', 'Sylhetsadar', 'Zakiganj', 'Dakshinsurma', 'Osmaninagar'],
+    Moulvibazar: ['Barlekha', 'Kamolganj', 'Kulaura', 'Moulvibazarsadar', 'Rajnagar', 'Sreemangal', 'Juri'],
+    Habiganj: ['Nabiganj', 'Bahubal', 'Ajmiriganj', 'Baniachong', 'Lakhai', 'Chunarughat', 'Habiganjsadar', 'Madhabpur', 'Shayestaganj'],
+    Sunamganj: ['Sadar', 'Southsunamganj', 'Bishwambarpur', 'Chhatak', 'Jagannathpur', 'Dowarabazar', 'Tahirpur', 'Dharmapasha', 'Jamalganj', 'Shalla', 'Derai', 'Madhyanagar'],
+    Narsingdi: ['Belabo', 'Monohardi', 'Narsingdisadar', 'Palash', 'Raipura', 'Shibpur'],
+    Gazipur: ['Kaliganj', 'Kaliakair', 'Kapasia', 'Sadar', 'Sreepur'],
+    Shariatpur: ['Sadar', 'Naria', 'Zajira', 'Gosairhat', 'Bhedarganj', 'Damudya'],
+    Narayanganj: ['Araihazar', 'Bandar', 'Narayanganjsadar', 'Rupganj', 'Sonargaon'],
+    Tangail: ['Basail', 'Bhuapur', 'Delduar', 'Ghatail', 'Gopalpur', 'Madhupur', 'Mirzapur', 'Nagarpur', 'Sakhipur', 'Tangailsadar', 'Kalihati', 'Dhanbari'],
+    Kishoreganj: ['Itna', 'Katiadi', 'Bhairab', 'Tarail', 'Hossainpur', 'Pakundia', 'Kuliarchar', 'Kishoreganjsadar', 'Karimgonj', 'Bajitpur', 'Austagram', 'Mithamoin', 'Nikli'],
+    Manikganj: ['Harirampur', 'Saturia', 'Sadar', 'Gior', 'Shibaloy', 'Doulatpur', 'Singiar'],
+    Dhaka: ['Savar', 'Dhamrai', 'Keraniganj', 'Nawabganj', 'Dohar'],
+    Munshiganj: ['Sadar', 'Sreenagar', 'Sirajdikhan', 'Louhajanj', 'Gajaria', 'Tongibari'],
+    Rajbari: ['Sadar', 'Goalanda', 'Pangsa', 'Baliakandi', 'Kalukhali'],
+    Madaripur: ['Sadar', 'Shibchar', 'Kalkini', 'Rajoir', 'Dasar'],
+    Gopalganj: ['Sadar', 'Kashiani', 'Tungipara', 'Kotalipara', 'Muksudpur'],
+    Faridpur: ['Sadar', 'Alfadanga', 'Boalmari', 'Sadarpur', 'Nagarkanda', 'Bhanga', 'Charbhadrasan', 'Madhukhali', 'Saltha'],
+    Panchagarh: ['Panchagarhsadar', 'Debiganj', 'Boda', 'Atwari', 'Tetulia'],
+    Dinajpur: ['Nawabganj', 'Birganj', 'Ghoraghat', 'Birampur', 'Parbatipur', 'Bochaganj', 'Kaharol', 'Fulbari', 'Dinajpursadar', 'Hakimpur', 'Khansama', 'Birol', 'Chirirbandar'],
+    Lalmonirhat: ['Sadar', 'Kaliganj', 'Hatibandha', 'Patgram', 'Aditmari'],
+    Nilphamari: ['Syedpur', 'Domar', 'Dimla', 'Jaldhaka', 'Kishorganj', 'Nilphamarisadar'],
+    Gaibandha: ['Sadullapur', 'Gaibandhasadar', 'Palashbari', 'Saghata', 'Gobindaganj', 'Sundarganj', 'Phulchari'],
+    Thakurgaon: ['Thakurgaonsadar', 'Pirganj', 'Ranisankail', 'Haripur', 'Baliadangi'],
+    Rangpur: ['Rangpursadar', 'Gangachara', 'Taragonj', 'Badargonj', 'Mithapukur', 'Pirgonj', 'Kaunia', 'Pirgacha'],
+    Kurigram: ['Kurigramsadar', 'Nageshwari', 'Bhurungamari', 'Phulbari', 'Rajarhat', 'Ulipur', 'Chilmari', 'Rowmari', 'Charrajibpur'],
+    Sherpur: ['Sherpursadar', 'Nalitabari', 'Sreebordi', 'Nokla', 'Jhenaigati'],
+    Mymensingh: ['Fulbaria', 'Trishal', 'Bhaluka', 'Muktagacha', 'Mymensinghsadar', 'Dhobaura', 'Phulpur', 'Haluaghat', 'Gouripur', 'Gafargaon', 'Iswarganj', 'Nandail', 'Tarakanda'],
+    Jamalpur: ['Jamalpursadar', 'Melandah', 'Islampur', 'Dewangonj', 'Sarishabari', 'Madarganj', 'Bokshiganj'],
+    Netrokona: ['Barhatta', 'Durgapur', 'Kendua', 'Atpara', 'Madan', 'Khaliajuri', 'Kalmakanda', 'Mohongonj', 'Purbadhala', 'Netrokonasadar']
+  };
+
   const allowedSchoolsByEducationLevel = {
     SSC: [
       "Admajee Cantonment High School",
@@ -293,7 +376,7 @@ function Update() {
     }
     setQueryStorage(newStorage);
     */
-   setQueryStorage(prevStorage => [...prevStorage, query]);
+    setQueryStorage(prevStorage => [...prevStorage, query]);
   };
 
   const handleSubmit = async () => {
@@ -314,29 +397,29 @@ function Update() {
     */
     const updateUserAPI = "http://localhost:8000/api/updateUser";
     queryStorage.map((query) => {
-        return fetch(updateUserAPI, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ query }),
-          })
-            .then((response) => {
-              if (!response.ok) {
-                throw new Error("Network response was not OK");
-              }
-              return response.json();
-            })
-            /*
-                  .then((data) => {
-                    loginData = data;
-                  })
-                  */
-            .catch((error) => {
-              console.error("Error fetching data:", error);
-            });
+      return fetch(updateUserAPI, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query }),
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not OK");
+          }
+          return response.json();
+        })
+        /*
+              .then((data) => {
+                loginData = data;
+              })
+              */
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
     });
-    
+
   };
   return (
     <div className="updateinfo-container">
@@ -447,12 +530,35 @@ function Update() {
       <label className="label">Address</label>
       <div className="input-container">
         <label className="label">District: </label>
-        <input
-          className="input-field"
+        <select
+          className="select-field"
           type="text"
           value={district}
-          onChange={(e) => setDistrict(e.target.value)}
-        />
+          onChange={(e) => {
+            const selectedDistrict = e.target.value;
+            setDistrict(selectedDistrict);
+            if (e.target.value === '') {
+              setIsThanaDisabled(true);
+              setThana(e.target.value);
+            }
+            else {
+              setIsThanaDisabled(false);
+            }
+            if (selectedDistrict in allowedThanaByDistrict) {
+              setAvailableThana(allowedThanaByDistrict[selectedDistrict]);
+            }
+            else {
+              setAvailableThana([]);
+            }
+          }}
+        >
+          <option value=''>Choose option</option>
+          {availableDistrict.map((district, index) => (
+            <option key={index} value={district}>
+              {district}
+            </option>
+          ))}
+        </select>
         <button
           onClick={() => handleUpdate("DISTRICT", district, setDistrict)}
           disabled={!district}
@@ -463,12 +569,22 @@ function Update() {
 
       <div className="input-container">
         <label className="label">Thana: </label>
-        <input
-          className="input-field"
-          type="text"
+        <select
+          className="select-field"
           value={thana}
-          onChange={(e) => setThana(e.target.value)}
-        />
+          onChange={(e) => {
+            setThana(e.target.value);
+          }}
+          disabled={isThanaDisabled}
+        >
+          <option value=''>Choose option</option> {
+            availableThana.map((thana, index) => (
+              <option key={index} value={thana}>
+                {thana}
+              </option>
+            ))
+          }
+        </select>
         <button
           onClick={() => handleUpdate("THANA", thana, setThana)}
           disabled={!thana}
@@ -660,23 +776,80 @@ function Update() {
         </button>
       </div>
 
+      <label className="label">Workplace address</label>
       <div className="input-container">
-        <label className="label">Location of workplace: </label>
-        <input
-          className="input-field"
-          type="text"
-          value={companyLocation}
-          onChange={(e) => setCompanyLocation(e.target.value)}
-        />
+        <label className="label">District: </label>
+        <select
+          className="select-field"
+          value={companyDistrict}
+          onChange={(e) => {
+            const selectedDistrict = e.target.value;
+            setCompanyDistrict(selectedDistrict);
+            if (e.target.value === '') {
+              setIsCompanyThanaDisabled(true);
+              setCompanyThana(e.target.value);
+            }
+            else {
+              setIsCompanyThanaDisabled(false);
+            }
+            if (selectedDistrict in allowedThanaByDistrict) {
+              setAvailableCompanyThana(allowedThanaByDistrict[selectedDistrict]);
+            }
+            else {
+              setAvailableCompanyThana([]);
+            }
+          }}
+        >
+          <option value=''>Choose option</option>
+          {availableDistrict.map((companyDistrict, index) => (
+            <option key={index} value={companyDistrict}>
+              {companyDistrict}
+            </option>
+          ))}
+        </select>
         <button
-          onClick={() =>
-            handleUpdate(
-              "COMPANY_LOCATION",
-              companyLocation,
-              setCompanyLocation
-            )
+          // onClick={() =>
+          //   handleUpdate(
+          //     "COMPANY_LOCATION",
+          //     companyLocation,
+          //     setCompanyLocation
+          //   )
+          // }
+          disabled={!companyDistrict}
+        >
+          Update
+        </button>
+      </div>
+
+      <div className="input-container">
+        <label className="label">Thana: </label>
+        <select
+          className="select-field"
+          value={companyThana}
+          onChange={(e) => {
+            setCompanyThana(e.target.value);
+            const temp = companyThana + ", " + companyDistrict;
+            setCompanyLocation(temp);
+          }}
+          disabled={isCompanyThanaDisabled}
+        >
+          <option value=''>Choose option</option> {
+            availableCompanyThana.map((companyThana, index) => (
+              <option key={index} value={companyThana}>
+                {companyThana}
+              </option>
+            ))
           }
-          disabled={!companyLocation}
+        </select>
+        <button
+          // onClick={() =>
+          //   handleUpdate(
+          //     "COMPANY_LOCATION",
+          //     companyLocation,
+          //     setCompanyLocation
+          //   )
+          // }
+          disabled={!companyThana}
         >
           Update
         </button>
