83f4df9ac82e4834bf03c236f39fd55b01f5c2ed
TowfiqueMahmud <towfiquemahmud6@gmail.com>
2023-09-12 04:40:57 +0600

    Revert "Dummy constraint of showing or not showing email and contact info handled"
    This reverts commit c1b00fd9915b134bf67abe3736423eec8118c6ab.

--- frontend/src/components/Spouse.js
+++ frontend/src/components/Spouse.js
@@ -5,7 +5,6 @@ function Spouse() {
   const location = useLocation();
   const matchData = location.state.index;
   const userid = location.state.userid;
-  let requested = false;//this value will be found out using query
 
   // Add a conditional check to ensure matchData exists before accessing its properties
   if (!matchData) {
@@ -16,18 +15,13 @@ function Spouse() {
   return (
     <div className="spouse-container">
       <h1>Hello user {userid}!</h1>
-      <h3>Suggested individual's information</h3>
+      <h3>Spouse Information</h3>
       <ul>
         <li>Name: {matchData.Name}</li>
         <li>Gender: {matchData.Gender}</li>
         <li>Birthdate: {matchData.DateOfBirth}</li>
-        {requested ? (
-          // Conditionally render Email and Contact if requested is true
-          <>
-            <li>Email: {matchData.Email}</li>
-            <li>Contact: {matchData.Contact}</li>
-          </>
-        ) : null}
+        <li>Email: {matchData.Email}</li>
+        <li>Contact: {matchData.Contact}</li>
         <li>Father: {matchData.Father}</li>
         <li>Mother: {matchData.Mother}</li>
         <li>Height: {matchData.Height}</li>
